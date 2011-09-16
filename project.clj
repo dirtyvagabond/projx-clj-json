a893de11c7aa36f91513ff3759fa4ab00c703d5b
Aaron Crow <dirtyvagabond@yahoo.com>
2011-09-16 01:14:27 -0700

    second commit

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject <%= name %> "1.0"
+  :description "FIXME: write"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+		 [org.danlarkin/clojure-json "1.2-SNAPSHOT"]])
---
+++ src/clj_json/example.clj
@@ -0,0 +1,5 @@
+(ns clj.json.example
+  (:require [org.danlarkin.json :as json]
+            [clojure.contrib.string :as str]))
+
+(println "HI!")
